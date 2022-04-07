3c35d32bae47e38ad7471dfdf03de668b229358a
Ivona Bogović <436581@student.fontys.nl>
2022-04-07 11:51:20 +0200

    Added User service

---
+++ BestReads/Authentication Service/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Authentication_Service.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
